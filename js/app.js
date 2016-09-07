596ecd34598d2f7631fe92501a2c7dbd5498015a
Sergio Gutiérrez <sergut18@gmail.com>
2016-09-07 17:37:05 -0500

    configured compass watch

---
+++ js/app.js
@@ -0,0 +1,3 @@
+var app = angular.module('Girgy', [
+  'ngMaterial',
+]);
---
+++ js/controllers/navController.js
@@ -0,0 +1,8 @@
+
+var volaires = angular.module('Girgy')
+
+volaires.controller('NavController', ['$scope', function($scope){
+
+    $scope.currentNavItem = 'page2';
+    
+}]);
