59c97ce9ce27c0de72e0bc76bebb8ecaa1bf6972
Sergio Gutiérrez <sergut18@gmail.com>
2016-10-19 11:42:04 -0500

    margen andd fixed UI

--- js/app.js
+++ js/app.js
@@ -9,33 +9,33 @@
 // });
 
 var app = angular.module('Girgy', [
-  'ngMaterial',
-  'ngRoute',
-  'Colombia',
-  'nvd3',
-  'ngFileUpload',
-  ]);
+    'ngMaterial',
+    'ngRoute',
+    'Colombia',
+    'nvd3',
+    'ngFileUpload',
+]);
 
 var volaires = angular.module('Girgy');
 
 volaires.config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('altTheme')
-  .primaryPalette('light-green')
-  .accentPalette('green')
-  .warnPalette('cyan')
-  .backgroundPalette('light-green');
+    $mdThemingProvider.theme('altTheme')
+        .primaryPalette('light-green')
+        .accentPalette('green')
+        .warnPalette('cyan')
+        .backgroundPalette('light-green');
 
-  $mdThemingProvider.theme('docs-dark', 'default')
-  .primaryPalette('light-green')
-  .backgroundPalette('green');
+    $mdThemingProvider.theme('docs-dark', 'default')
+        .primaryPalette('light-green')
+        .backgroundPalette('green');
 
-  $mdThemingProvider.theme('green')
-  .primaryPalette('green')
-  .accentPalette('light-green')
-  .warnPalette('green');
+    $mdThemingProvider.theme('green')
+        .primaryPalette('green')
+        .accentPalette('light-green')
+        .warnPalette('orange');
 
-  $mdThemingProvider.theme('amber')
-  .primaryPalette('amber')
-  .accentPalette('orange')
-  .warnPalette('red');
+    $mdThemingProvider.theme('amber')
+        .primaryPalette('amber')
+        .accentPalette('orange')
+        .warnPalette('red');
 })
